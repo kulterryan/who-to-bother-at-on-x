28f2b6104a3a7edb649f03019ad1c80818ab83b3
Aryan Chaurasia <chaurasia.aryan@yahoo.com>
2025-10-31 00:04:05 +0530

    init cloudflare deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     // Enables Vite to resolve imports using path aliases.
     tsconfigPaths(),
