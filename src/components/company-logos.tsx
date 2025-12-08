26659fc2075f226add228c8749fc0edd6a260ee5
Aryan Chaurasia <chaurasia.aryan@yahoo.com>
2025-12-09 02:30:48 +0530

    fixed: drizzle logo

--- src/components/company-logos.tsx
+++ src/components/company-logos.tsx
@@ -283,6 +283,11 @@ export const companyLogos: Record<string, React.ReactNode> = {
     </svg>
   ),
   drizzle: (
-    <svg fill="none" viewBox="0 0 160 160"><rect width="9.631" height="40.852" fill="currentColor" rx="4.816" transform="matrix(.87303 .48767 -.49721 .86763 43.48 67.304)"/><rect width="9.631" height="40.852" fill="#121212" rx="4.816" transform="matrix(.87303 .48767 -.49721 .86763 76.94 46.534)"/><rect width="9.631" height="40.852" fill="#121212" rx="4.816" transform="matrix(.87303 .48767 -.49721 .86763 128.424 46.535)"/><rect width="9.631" height="40.852" fill="#121212" rx="4.816" transform="matrix(.87303 .48767 -.49721 .86763 94.957 67.304)"/></svg>
-  )
+    <svg fill="none" width="42" height="42" viewBox="0 0 160 160" className="text-zinc-900 dark:text-zinc-100" xmlns="http://www.w3.org/2000/svg">
+      <rect width="9.631" height="40.852" fill="currentColor" rx="4.816" transform="matrix(.87303 .48767 -.49721 .86763 43.48 67.304)" />
+      <rect width="9.631" height="40.852" fill="currentColor" rx="4.816" transform="matrix(.87303 .48767 -.49721 .86763 76.94 46.534)" />
+      <rect width="9.631" height="40.852" fill="currentColor" rx="4.816" transform="matrix(.87303 .48767 -.49721 .86763 128.424 46.535)" />
+      <rect width="9.631" height="40.852" fill="currentColor" rx="4.816" transform="matrix(.87303 .48767 -.49721 .86763 94.957 67.304)" />
+    </svg>
+  ),
 };
